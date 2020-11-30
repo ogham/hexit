4293f92f8017626840d8d0380fae6b5a0117b537
Benjamin Sago <ogham@bsago.me>
2020-11-30 18:20:00 +0000

    Timestamps must have an endianness

--- hexit-lang/src/eval.rs
+++ hexit-lang/src/eval.rs
@@ -22,21 +22,7 @@ fn eval<'src>(exps: impl IntoIterator<Item=Exp<'src>>, constants: &ConstantsTabl
 
     for exp in exps {
         let val = evaluator.evaluate_exp(exp)?;
-
-        if let Value::RawNumber(s) = val {
-            match s.parse() {
-                Ok(v) => {
-                    bytes.push(v)
-                }
-                Err(e) => {
-                    warn!("Parse error: {}", e);
-                    return Err(Error::TopLevelBigDecimal(s))
-                }
-            }
-        }
-        else {
-            bytes.extend(val.eval_to_bytes());
-        }
+        bytes.extend(val.eval_to_bytes()?);
     }
 
     Ok(bytes)
@@ -47,34 +33,58 @@ fn eval<'src>(exps: impl IntoIterator<Item=Exp<'src>>, constants: &ConstantsTabl
 #[derive(PartialEq, Debug)]
 enum Value<'src> {
     Byte(u8),
+    VariableBytes(Vec<u8>),
+    MultiByte(MultiByteValue<'src>),
+}
+
+#[derive(PartialEq, Debug)]
+pub enum MultiByteValue<'src> {
     Sixteen(u16),
     ThirtyTwo(u32),
     SixtyFour(u64),
-    VariableBytes(Vec<u8>),
     RawNumber(&'src str),
 }
 
-impl Value<'_> {
-    fn eval_to_bytes(self) -> Vec<u8> {
+impl<'src> Value<'src> {
+    fn eval_to_bytes(self) -> Result<Vec<u8>, Error<'src>> {
         match self {
             Self::Byte(byte) => {
-                vec![ byte ]
+                Ok(vec![ byte ])
             }
             Self::VariableBytes(bytes) => {
-                bytes
+                Ok(bytes)
             }
-            Self::Sixteen(_) | Self::ThirtyTwo(_) | Self::SixtyFour(_) | Self::RawNumber(_) => {
-                panic!("top level number")
+            Self::MultiByte(MultiByteValue::RawNumber(s)) => {
+                match s.parse() {
+                    Ok(v) => {
+                        Ok(vec![ v ])
+                    }
+                    Err(e) => {
+                        warn!("Parse error: {}", e);
+                        Err(Error::TopLevelBigDecimal(MultiByteValue::RawNumber(s)))
+                    }
+                }
+            }
+            Self::MultiByte(v) => {
+                Err(Error::TopLevelBigDecimal(v))
             }
         }
     }
 
     fn to_two_variable_bytes(self, endianify: impl Fn(u16) -> [u8; 2]) -> Self {
         let bytes = match self {
-            Self::Byte(b)       => endianify(u16::from(b)),
-            Self::Sixteen(o2)   => endianify(u16::from(o2)),
-            Self::RawNumber(s)  => endianify(u16::from_str_radix(s, 10).unwrap()),
-            _                   => todo!("val: {:?}", self),
+            Self::Byte(b) => {
+                endianify(u16::from(b))
+            }
+            Self::MultiByte(MultiByteValue::Sixteen(o2)) => {
+                endianify(u16::from(o2))
+            }
+            Self::MultiByte(MultiByteValue::RawNumber(s)) => {
+                endianify(u16::from_str_radix(s, 10).unwrap())
+            }
+            _ => {
+                todo!("val: {:?}", self)
+            }
         };
 
         Value::VariableBytes(bytes.to_vec())
@@ -82,10 +92,21 @@ impl Value<'_> {
 
     fn to_four_variable_bytes(self, endianify: impl Fn(u32) -> [u8; 4]) -> Self {
         let bytes = match self {
-            Self::Byte(b)       => endianify(u32::from(b)),
-            Self::Sixteen(o2)   => endianify(u32::from(o2)),
-            Self::RawNumber(s)  => endianify(u32::from_str_radix(s, 10).unwrap()),
-            _                   => todo!("val: {:?}", self),
+            Self::Byte(b) => {
+                endianify(u32::from(b))
+            }
+            Self::MultiByte(MultiByteValue::Sixteen(o2)) => {
+                endianify(u32::from(o2))
+            }
+            Self::MultiByte(MultiByteValue::ThirtyTwo(o4)) => {
+                endianify(o4)
+            }
+            Self::MultiByte(MultiByteValue::RawNumber(s)) => {
+                endianify(u32::from_str_radix(s, 10).unwrap())
+            }
+            _ => {
+                todo!("val: {:?}", self)
+            }
         };
 
         Value::VariableBytes(bytes.to_vec())
@@ -93,10 +114,18 @@ impl Value<'_> {
 
     fn to_eight_variable_bytes(self, endianify: impl Fn(u64) -> [u8; 8]) -> Self {
         let bytes = match self {
-            Self::Byte(b)       => endianify(u64::from(b)),
-            Self::Sixteen(o2)   => endianify(u64::from(o2)),
-            Self::RawNumber(s)  => endianify(u64::from_str_radix(s, 10).unwrap()),
-            _                   => todo!("val: {:?}", self),
+            Self::Byte(b) => {
+                endianify(u64::from(b))
+            }
+            Self::MultiByte(MultiByteValue::Sixteen(o2)) => {
+                endianify(u64::from(o2))
+            }
+            Self::MultiByte(MultiByteValue::RawNumber(s)) => {
+                endianify(u64::from_str_radix(s, 10).unwrap())
+            }
+            _ => {
+                todo!("val: {:?}", self)
+            }
         };
 
         Value::VariableBytes(bytes.to_vec())
@@ -121,7 +150,7 @@ impl<'consts> Evaluator<'consts> {
             }
 
             Exp::Dec(number) => {
-                Ok(Value::RawNumber(number))
+                Ok(Value::MultiByte(MultiByteValue::RawNumber(number)))
             }
 
             Exp::Constant { name } => {
@@ -130,7 +159,7 @@ impl<'consts> Evaluator<'consts> {
                         Ok(Value::Byte(num))
                     }
                     Ok(Constant::Sixteen(num)) => {
-                        Ok(Value::Sixteen(num))
+                        Ok(Value::MultiByte(MultiByteValue::Sixteen(num)))
                     }
                     Err(e) => {
                         return Err(Error::UnknownConstant(e));
@@ -156,7 +185,7 @@ impl<'consts> Evaluator<'consts> {
             }
 
             Exp::Timestamp(unix_time) => {
-                Ok(Value::VariableBytes(unix_time.to_be_bytes().to_vec()))
+                Ok(Value::MultiByte(MultiByteValue::ThirtyTwo(unix_time)))
             }
         }
     }
@@ -237,10 +266,10 @@ pub enum Error<'src> {
 
     /// A decimal number was too big for a byte at the top level, such as
     /// `[9999999]`.
-    TopLevelBigDecimal(&'src str),
+    TopLevelBigDecimal(MultiByteValue<'src>),
 
     /// A decimal number was too big for its target, such as `be16[99999999]`.
-    TooBigDecimal(&'src str),
+    TooBigDecimal(MultiByteValue<'src>),
 
     /// A constant value was referenced that does not exist.
     UnknownConstant(UnknownConstant),
@@ -306,6 +335,6 @@ mod test {
     fn top_level_decimal_256() {
         let exps = vec![ Exp::Dec("256") ];
         assert_eq!(evaluate_exps(exps, &ConstantsTable::empty(), None),
-                   Err(Error::TopLevelBigDecimal("256")));
+                   Err(Error::TopLevelBigDecimal(MultiByteValue::RawNumber("256"))));
     }
 }
--- hexit-lang/tests/line/form_tests.rs
+++ hexit-lang/tests/line/form_tests.rs
@@ -2,4 +2,5 @@ test_eval!(localhost: "[127.0.0.1]"            => Ok(vec![ 127, 0, 0, 1 ]));
 test_eval!(broadcast: "[255.255.255.255]"      => Ok(vec![ 255, 255, 255, 255 ]));
 test_eval!(ipv6:      "[::1]"                  => Ok(vec![ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ]));
 
-test_eval!(timestamp: "[2017-12-31T21:36:45]" => Ok(vec![ 0x5A, 0x49, 0x58, 0x6D ]));
+test_eval!(le32_timestamp: "le32[2017-12-31T21:36:45]" => Ok(vec![ 0x6D, 0x58, 0x49, 0x5A ]));
+test_eval!(be32_timestamp: "be32[2017-12-31T21:36:45]" => Ok(vec![ 0x5A, 0x49, 0x58, 0x6D ]));
